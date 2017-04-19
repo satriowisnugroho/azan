16d8687a47a2c14430e2e2d81f3346cba4166f59
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-04-19 21:07:26 +0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const request = require('request');
+const cheerio = require('cheerio');
+const moment = require('moment');
+require('colors');
+
+let cityId = 307;
+let url = `http://www.jadwalsholat.org/adzan/ajax/ajax.daily1.php?id=${cityId}`;
+
+request(url, (err, res, body) => {
+  if (err) throw err;
+  azan.load(body);
+});
+
+const azan = {
+  load: (body) => {
+    let data = azan.getData(cheerio.load(body));
+    azan.print(data);
+  },
+  getData: $ => {
+    let data = [], found = null;
+
+    $('tr').each((i, value) => {
+      if (i < 2 || i > 6) return;
+
+      let td = $(value).find('td');
+
+      data.push({
+        name: td.eq(0).text(),
+        time: td.eq(1).text(),
+        diff: moment(td.eq(1).text(), 'HH:mm').diff(moment(), 'second'),
+      });
+    });
+
+    data.some(val => {
+      if (val.diff >= 0) {
+        found = val;
+        return true;
+      }
+    });
+
+    return {
+      azans: data,
+      found: found
+    };
+  },
+  print: data => {
+    let found = data.found;
+
+    console.log(`\n.:| JADWAL SHOLAT HARI INI ${moment().format('DD MMMM YYYY')} |:.\n`.red);
+
+    if (found) {
+      let time = azan.setTime(found);
+      console.log(`Azan sholat ${found.name} pukul ${found.time} tinggal ${time.yellow} lagi\n`);
+    }
+
+    data.azans.forEach(val => {
+      console.log(`${val.name.blue}\t${val.time.green}`);
+    });
+
+    console.log('\n');
+  },
+  setTime: found => {
+    let time = '';
+    let hour = Math.floor(found.diff / 60 / 60);
+
+    time += hour !== 0 ? `${hour} jam ` : '';
+    time += Math.floor(found.diff / 60 % 60) + ' menit';
+
+    return time;
+  }
+};
