961794f998468830cb9c8fbf17017a73a5c9a2d6
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-04-22 08:18:27 +0700

    CityId config.

--- index.js
+++ index.js
@@ -2,12 +2,50 @@
 
 'use strict';
 
+require('colors');
 const request = require('request');
 const cheerio = require('cheerio');
 const moment = require('moment');
-require('colors');
+const assert = require('assert');
+const env = require('node-env-file');
+const cities = require('./cities');
+const fs = require('fs');
 
-let cityId = 307;
+let cityList = cities.map(city => {
+  return city.id + ' = ' + city.name;
+});
+
+let flag = process.argv[2];
+if (flag === '--help' || flag === '-h') {
+  console.log('Usage: azan [options]\n');
+  console.log('Options:\n  --city: City id e.g. `307`\n');
+  console.log('Cities:');
+  console.log(JSON.stringify(cityList, null, 1));
+  process.exit();
+} else if (flag && flag.split('=')[0] === '--city') {
+  let city = cities.find(city => {
+    return city.id === parseInt(flag.split('=')[1]);
+  });
+  
+  if (typeof city === 'undefined') {
+    console.log('City not found')
+  } else {
+    try {
+      fs.writeFileSync(__dirname + '/.env', `CITY_ID=${city.id}`, 'utf8');
+      console.log(`Configuration Successful\nCity: ${city.id} - ${city.name}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  process.exit();
+}
+
+env(__dirname + '/.env');
+
+let cityId = process.env.CITY_ID;
+let city = cities.find(city => {
+  return city.id === parseInt(cityId);
+});
 let url = `http://www.jadwalsholat.org/adzan/ajax/ajax.daily1.php?id=${cityId}`;
 
 request(url, (err, res, body) => {
@@ -50,11 +88,11 @@ const azan = {
   print: data => {
     let found = data.found;
 
-    console.log(`\n.:| JADWAL SHOLAT HARI INI ${moment().format('DD MMMM YYYY')} |:.\n`.red);
+    console.log(`\n.:| ${city.name} - ${moment().format('DD MMMM YYYY')} |:.\n`);
 
     if (found) {
       let time = azan.setTime(found);
-      console.log(`Azan sholat ${found.name} pukul ${found.time} tinggal ${time.yellow} lagi\n`);
+      console.log(`Azan ${found.name} at ${found.time} - ${time.yellow} left\n`);
     }
 
     data.azans.forEach(val => {
@@ -67,8 +105,8 @@ const azan = {
     let time = '';
     let hour = Math.floor(found.diff / 60 / 60);
 
-    time += hour !== 0 ? `${hour} jam ` : '';
-    time += Math.floor(found.diff / 60 % 60) + ' menit';
+    time += hour !== 0 ? `${hour} h ` : '';
+    time += Math.floor(found.diff / 60 % 60) + ' m';
 
     return time;
   }
